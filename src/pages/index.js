bba900eb69bd21e0d40aa0489c4ef8e9344627bc
Pawel L <virtuozoo@o2.pl>
2020-10-21 12:47:23 +0200

    add layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,34 +1,15 @@
 import React from "react"
-import Layout from "../components/layout"
-import PhotoGallery from "../components/PhotoGallery"
-import { graphql } from 'gatsby';
+import Layout from "../components/Layout"
 
 
-const IndexPage = ({data}) => {
-  const galleryPhotos = data.allDatoCmsGalleryphoto.edges
+
+const IndexPage = () => {
   return (
     <Layout>
-    <PhotoGallery galleryPhotos={galleryPhotos}/>
+      <h2>Hello World</h2>
   </Layout>
   )
 };
 
-
-export const query = graphql`
-query {
-  allDatoCmsGalleryphoto {
-    edges {
-      node {
-        id
-        title
-        category
-        events {
-          url
-        }
-      }
-    }
-  }
-
-}`;
 
 export default IndexPage
