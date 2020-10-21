bc0a907d9043dfbd25b56bafc91c9486b7707de2
Pawel L <virtuozoo@o2.pl>
2020-10-21 18:09:57 +0200

    add intro section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,23 @@
 import React from "react"
 import Layout from "../components/Layout"
+import '../template/style.scss';
+import introBg from "../assets/intro-img.jpg"
 
 
 
 const IndexPage = () => {
   return (
     <Layout>
-      <h2>Hello World</h2>
+      <div className="intro">
+        <header className="intro__content">
+          <h1 className="intro__title">Witam cie na mojej stronie</h1>
+          <p className="intro__desc">Mam na imię Michał, a fotografia to moja najwieksza pasja.</p>
+          <a className="intro__btn" href="#">Portfolio</a>
+        </header>
+        <div className="intro__image">
+          <img src={introBg} alt="" />
+        </div>
+      </div>
     </Layout>
   )
 };
