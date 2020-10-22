345bd6278c71e8bfba49b7932bd937a6b4ea3d2c
Pawel L <virtuozoo@o2.pl>
2020-10-22 18:31:26 +0200

    create about section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Layout from "../components/Layout"
 import '../pages/index.scss';
 import introBg from "../assets/intro-img.jpg"
 import portfolioLink from '../assets/portfolio-link.jpg'
+import aboutImg from '../assets/about-img.jpg'
 
 
 
@@ -43,6 +44,31 @@ const IndexPage = () => {
                 <img src={portfolioLink} alt="" />
               </a>
             </div>
+          </div>
+        </div>
+      </section>
+      <section className="about">
+        <div className="wrapper">
+          <div className="about__content">
+            <h2 className="about__title">Michał Wroceński</h2>
+            <p className="about__desc">
+              Mam na imię Michał, a fotografia to moja największa pasja.
+              Pasja, którą postanowiłem uczynić swoją profesją.
+            </p>
+            <p className="about__desc">
+              Każde naciśnięcie migawki, każde "pstryk" zrobione przez
+              aparat to dla mnie wyjątkowy moment. Moment, w którym
+              uwieczniłem na zdjęciu ułamek chwili.
+            </p>
+            <p className="about__desc">
+              Czy to podczas pozowanej sesji, czy podczas tak ważnego dla Was wydarzenia
+              jak Wasz ślub. Każde zdjęcie jest wyjątkowe, bo zatrzymuje chwilę.
+              Będzie mi niezmierni miło, jeśli wybierzesz właśnie mnie,
+              abym uwiecznił chwilę z Twojego życia na fotografii.
+            </p>
+          </div>
+          <div className="about__image">
+            <img src={aboutImg} alt="" />
           </div>
         </div>
       </section>
