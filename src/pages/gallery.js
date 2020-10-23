fb96884152a5620b6c9df9b09da16536a188e722
Pawel L <virtuozoo@o2.pl>
2020-10-23 17:45:19 +0200

    add basic styles and html for gallery

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,12 +1,65 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import Layout from '../components/Layout'
+import galleryImg from '../assets/gallery.jpeg';
 
 
 const GalleryPage = () => {
     return (
         <Layout>
-            <div className="wrapper">
-                <h2>lorem</h2>
+            <div className="gallery">
+                <div className="wrapper">
+                    <h2 className="gallery__title">Zdjęcia</h2>
+                    <nav className="gallery__nav">
+                        <ul className="gallery__list">
+                            <li className="gallery__category">
+                                <Link className="gallery__link" href="#">Wszystko</Link>
+                            </li>
+                            <li className="gallery__category">
+                                <Link className="gallery__link" href="#">Portrety</Link>
+                            </li>
+                            <li className="gallery__category">
+                                <Link className="gallery__link" href="#">Eventy</Link>
+                            </li>
+                            <li className="gallery__category">
+                                <Link className="gallery__link" href="#">Wesela</Link>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <div className="gallery__container">
+                        <div className="gallery__item">
+                            <img className="gallery__image" src={galleryImg} alt="" />
+                        </div>
+                        <div className="gallery__item">
+                            <img className="gallery__image" src={galleryImg} alt="" />
+                        </div>
+                        <div className="gallery__item">
+                            <img className="gallery__image" src={galleryImg} alt="" />
+                        </div>
+                        <div className="gallery__item">
+                            <img className="gallery__image" src={galleryImg} alt="" />
+                        </div>
+                        <div className="gallery__item">
+                            <img className="gallery__image" src={galleryImg} alt="" />
+                        </div>
+                        <div className="gallery__item">
+                            <img className="gallery__image" src={galleryImg} alt="" />
+                        </div>
+                        <div className="gallery__item">
+                            <img className="gallery__image" src={galleryImg} alt="" />
+                        </div>
+                        <div className="gallery__item">
+                            <img className="gallery__image" src={galleryImg} alt="" />
+                        </div>
+                        <div className="gallery__item">
+                            <img className="gallery__image" src={galleryImg} alt="" />
+                        </div>
+                        <div className="gallery__item">
+                            <img className="gallery__image" src={galleryImg} alt="" />
+                        </div>
+                    </div>
+                </div>
             </div>
         </Layout >
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -150,7 +150,6 @@ const IndexPage = () => {
   )
 };
 
-
 export default IndexPage
 
 
