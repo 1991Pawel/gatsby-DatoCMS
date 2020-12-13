2213b4fb7494a400234a25f006f28ef715bd909e
Pawel L <virtuozoo@o2.pl>
2020-12-13 13:54:08 +0100

    add handleKeyPress to open modal with enter

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { graphql } from 'gatsby'
 import Img from "gatsby-image"
 
@@ -14,14 +14,15 @@ const tabs = [
     { content: 'Wesela', path: '/weeding', id: 'wedding' },
 ]
 
-const GalleryList = ({ item, setSelectedImg }) => {
+const GalleryList = ({ item, setSelectedImg, handleUserKeyPress }) => {
     return (
         <motion.li
-            tabIndex="0"
+            onKeyUp={(e) => handleUserKeyPress(e, item.node.events[0].fluid)}
             onClick={() => setSelectedImg(item.node.events[0].fluid)}
             initial={{ opacity: 0, scale: .8 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: .6 }}
+            tabIndex="0"
             className="gallery__item">
             <Img className="gallery__image" fluid={item.node.events[0].fluid} />
             <span>{item.node.title}</span>
@@ -51,6 +52,14 @@ const GalleryPage = (props) => {
         setFilter(id);
     }
 
+    const handleUserKeyPress = useCallback((e, image) => {
+        console.log(e, image)
+        const { key } = e;
+        if (key === 'Enter') {
+            setSelectedImg(image)
+        }
+    }, []);
+
     return (
         <Layout >
             {selectedImg && <Modal setSelectedImg={setSelectedImg} selectedImg={selectedImg} />}
@@ -73,7 +82,7 @@ const GalleryPage = (props) => {
                         </ul>
                     </nav>
                     <ul className="gallery__container">
-                        {filterList.length ? filterList.map((item) => <GalleryList setSelectedImg={setSelectedImg} key={item.node.id} item={item} />) : <p>Brak zdjęć</p>}
+                        {filterList.length ? filterList.map((item) => <GalleryList handleUserKeyPress={handleUserKeyPress} setSelectedImg={setSelectedImg} key={item.node.id} item={item} />) : <p>Brak zdjęć</p>}
                     </ul>
                 </div>
             </div>
