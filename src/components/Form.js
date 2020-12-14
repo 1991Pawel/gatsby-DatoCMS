8ca5b23e780230ac766b53c462d8d42097455955
Pawel L <virtuozoo@o2.pl>
2020-12-14 20:05:07 +0100

    fix action

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 
 const Form = () => (
-<form name="contact" method="POST" data-netlify="true">
+<form name="contact" action="POST" data-netlify="true">
   <p>
     <label>Your Name: <input type="text" name="name" /></label>   
   </p>
