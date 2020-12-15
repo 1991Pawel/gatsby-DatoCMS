8b6c64afdf42a2a0eb4c569c6b61f094d0edc8a9
Pawel L <virtuozoo@o2.pl>
2020-12-15 03:18:24 +0100

    add basic form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,9 +7,10 @@ import instagramIcon from '../assets/facebook.svg'
 const Footer = () => (
     <footer id="contact" className="contact">
         <div className="wrapper">
-            <div className="socials">
-                <h4>Kontakt</h4>
+         <h2 className="contact__title">Kontakt</h2>
+            <div className="socials">   
                 <Form/>
+                <div className="socials__inner">
                 <ul className="socials__list">
                     {/* <li className="socials__item">
                         <a className="socials__link" href="tel:+18506484200">
@@ -23,19 +24,20 @@ const Footer = () => (
                     </li>
                 </ul>
                 <h4>znajdz mnie</h4>
-                <ul className="socials__list socials__list--flex">
 
-                    <li className="socials__item socials__item--mr">
-                        <a className="socials__link" target="_blank" rel="noreferrer" href="https://www.instagram.com/wrocenskifoto">
-                            <img className="socials__icon" src={facebookIcon} alt="facebook icon" />
-                        </a>
-                    </li>
-                    <li className="socials__item socials__item--mr">
-                        <a className="socials__link" target="_blank" rel="noreferrer" href="https://www.facebook.com/WrocenskiFoto">
-                            <img className="socials__icon" src={instagramIcon} alt="instagram icon" />
-                        </a>
-                    </li>
-                </ul>
+           <ul className="socials__list socials__list--flex">
+                <li className="socials__item socials__item--mr">
+                    <a className="socials__link" target="_blank" rel="noreferrer" href="https://www.instagram.com/wrocenskifoto">
+                        <img className="socials__icon" src={facebookIcon} alt="facebook icon" />
+                    </a>
+                </li>
+                <li className="socials__item socials__item--mr">
+                    <a className="socials__link" target="_blank" rel="noreferrer" href="https://www.facebook.com/WrocenskiFoto">
+                        <img className="socials__icon" src={instagramIcon} alt="instagram icon" />
+                    </a>
+                </li>
+            </ul>
+           </div>
             </div>
         </div>
         <p className="copyright"><a href="http://pavdev.pl">© 2020 P.L All Rights Reserved</a></p>
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,107 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import './Form.scss';
 
+const initialState = {
+    name: '',
+    email: '',
+    message: '',
+}
 
-const Form = () => (
-    <form
-      name="contact-form"
-      method="post"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-    >
-      <input name="name" placeholder="imie" type="text" />
-      <input name="email" placeholder="email@adress.com" type="email" />
-      <textarea name="message" placeholder="text" />
-      <button type="submit">Wyślij</button>
-    </form>
+const Form = () => {
+    const [values, setValues] = useState(initialState);
+    const [error,setError] = useState(false);
+    const [success,setSucess] = useState(false);
 
-)
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setError(false)
+            setSucess(false)    
+        }, 4000);
+        return () => {
+            clearTimeout(timer)
+          }
+    }, [success,error])
+
+    const handleSubmit = e => {
+        setError(false);   
+        e.preventDefault();
+
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...values })
+          })
+          .then((response) => {
+              console.log('wysłana')
+              setError(() => false)
+              setSucess(true)
+          })
+          .catch(error => {
+            console.log('nie wyslana')
+              setError(() => true);
+              setSucess(false)
+          })  
+
+          setValues(initialState);   
+      };
+  
+      const handleChange = (e) => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    const encode = (data) => {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&");
+    }
+
+  
+
+    return (
+        <form
+            className="form"
+            name="contact-form"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
+            onSubmit={handleSubmit}
+        >
+            <div className="form__inner">
+                    {error ? <p className="form__error">Błąd wysyłania</p> : null}
+                    {success ? <p className="form__success">Wiadomość wysłana</p> : null}
+                    <label 
+                    className="form__label" 
+                    htmlFor="name">Imię</label>
+                    <input 
+                    onChange={(e) => handleChange(e)} 
+                    className="form__input" 
+                    type="text" id="name" 
+                    name="name" 
+                    value={values.name} 
+                    required/>
+                    <label className="form__label" htmlFor="mail">Email</label>
+                    <input 
+                    onChange={(e) => handleChange(e)}
+                    className="form__input" 
+                    type="email" id="mail" 
+                    name="email"
+                    value={values.email} 
+                    required/>
+                    <label className="form__label" htmlFor="message">Wiadomość</label>
+                    <textarea 
+                    onChange={(e) => handleChange(e)}  
+                    className="form__textarea" 
+                    rows="4" cols="50" 
+                    name="message" 
+                    value={values.message}
+                    required>
+                    </textarea>
+                    <button className="form__btn" type="submit">Wyślij</button>
+            </div>
+        </form>
+    )
+}
 export default Form;
