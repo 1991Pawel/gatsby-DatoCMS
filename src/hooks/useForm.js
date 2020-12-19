cdf60b5e5141da45c5924bfdaa25b527304cd621
Pawel L <virtuozoo@o2.pl>
2020-12-19 14:44:04 +0100

    remove React from useForm, add css to Form component

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,4 +1,4 @@
-import React, {useEffect,useState} from 'react'
+import {useEffect,useState} from 'react'
 
 export const useForm  = (initialState) => {
     const [values, setValues] = useState(initialState);
